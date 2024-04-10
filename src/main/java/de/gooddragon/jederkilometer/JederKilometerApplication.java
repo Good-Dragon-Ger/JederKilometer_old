e929f44b6edc18437fa8c7a869e510e6a2bb1dd8
dragonempire96 <maximilian.mleziva@gmx.de>
2024-04-10 11:41:59 +0200

    init

---
+++ src/main/java/de/gooddragon/jederkilometer/JederKilometerApplication.java
@@ -0,0 +1,13 @@
+package de.gooddragon.jederkilometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JederKilometerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JederKilometerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/gooddragon/jederkilometer/JederKilometerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gooddragon.jederkilometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JederKilometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
